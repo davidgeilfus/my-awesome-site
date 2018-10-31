3ad48b6f62c9424ea0ce3e80e2e647d1780bc832
David Geilfus <davidgeilfus@gmail.com>
2018-10-31 10:46:30 +0100

    Configured CircleCI (WIP)

---
+++ automated_build.sh
@@ -0,0 +1,24 @@
+# Copy static site
+CWD=`pwd`
+
+# Clone Pages repository
+cd /tmp
+git clone YOUR_PAGES_REPO build
+# cd build && git checkout -b YOUR_BRANCH origin/YOUR_BRANCH # If not using master
+
+# Trigger Jekyll rebuild
+cd $CWD
+bundle exec jekyll contentful
+bundle exec jekyll build
+
+# Push newly built repository
+cp -r $CWD/_build/* /tmp/build # or $CWD/_site
+
+cd /tmp/build
+
+git config --global user.email "davidgeilfus+github@gmail.com"
+git config --global user.name "David Geilfus"
+
+git add .
+git commit -m "Automated Rebuild"
+git push -f origin YOUR_PAGES_BRANCH
